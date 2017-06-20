9945b489c949dfb0f7f0f1e26a630b25ee9e01d8
ponoboy <kazuboi30@gmail.com>
2017-06-19 14:58:12 -1000

    Not Finished

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 function fortuneTeller(){
-	console.log("Hello")
-}
+	var numberOfChildren = 4;
+	var partnersName = "Sienna"
+	var geographicLocation = "Kaneohe" 
+	var jobTitle = "3D Game Devolper"
+	console.log("You will be a X in Y, and married to Z with N kids.")
+}	console.log("You will be a " + jobTitle + "in" + geographicLocation +", and married to + partnersName + ")
 
 function ageCalculator(){
+var birthyear = "2004"
+var futureyear = "2020"
 
 } 
+
+ageCalculator();
